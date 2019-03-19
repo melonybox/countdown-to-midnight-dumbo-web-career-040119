17c02e6a2cb4df321bdfc1e5ed5f4391346a2fb8
Gino Desangles <blushingbox@yahoo.com>
2019-03-19 03:23:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = num
+  until counter == 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  counter = num
+  until counter == 0
+    counter -= 1
+    sleep 1
+  end
 end
