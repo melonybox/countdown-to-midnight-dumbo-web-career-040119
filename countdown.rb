d5fbc8e141bd436bf718d4bc3ba88398fb91f781
Gino Desangles <blushingbox@yahoo.com>
2019-03-19 03:15:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,11 @@
 
 def countdown(num)
   counter = num
-  until counter == 0
-  puts 
+  until counter == 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep 1
+  end
+  sleep 1
+  puts "HAPPY NEW YEAR!"
 end
